626fa63f7acc92fce9d594eb939f89809fac4f94
mahmoud khaled <54672453+mahmoudkahled@users.noreply.github.com>
2024-10-13 00:46:07 +0300

    feat: add the LLM model implementation

--- src/model.py
+++ src/model.py
@@ -3,8 +3,13 @@ from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
 )
+from typing import Tuple
+from transformers import pipeline
+from langchain.llms import HuggingFacePipeline
 
-def load_model(model_name = CFG.model_name):
+def load_model(
+    model_name: str= CFG.model_name
+) -> Tuple[AutoTokenizer, AutoModelForCausalLM, int]:
     if model_name == 'Qwen2.5':
         model_repo = 'Qwen/Qwen2.5-0.5B-Instruct'
         
@@ -22,3 +27,24 @@ def load_model(model_name = CFG.model_name):
         raise NotImplementedError("Not implemented model (tokenizer and backbone)")
 
     return tokenizer, model, max_len
+
+def create_llm(
+    model: AutoModelForCausalLM,
+    tokenizer: AutoTokenizer,
+    max_len: int,
+    config: CFG
+) -> HuggingFacePipeline:
+    pipe = pipeline(
+        task = "text-generation",
+        model = model,
+        tokenizer = tokenizer,
+        pad_token_id = tokenizer.eos_token_id,
+        max_new_tokens= max_len,
+        do_sample = False,
+        top_p = config.top_p,
+        repetition_penalty = config.repetition_penalty
+    )
+
+    llm = HuggingFacePipeline(pipeline = pipe)
+
+    return llm
